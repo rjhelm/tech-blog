15a8dccf1fee5d0cda7d60f097667caf9b74f56e
Ryan Helm <rjhelm@github.io>
2021-04-28 21:16:43 -0600

    seed data for comments

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,4 +4,19 @@ const commentData = [{
     comment_text: "Lorem ipsum sequdn lors gofs",
     user_id: 1,
     post_id: 1
-}]
+    },
+    {
+        comment_text: "filleouj kilsdd kdfor",
+        user_id: 2,
+        post_id: 2
+    },
+    {
+        comment_text: "dees od sdein ut labore et dolore magna aliqua",
+        user_id: 3,
+        post_id: 3
+    }
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
