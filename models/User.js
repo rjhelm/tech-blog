f546ed70a0b111edc376bacea80b10773b5af97c
Ryan Helm <rjhelm@github.io>
2021-04-29 17:58:52 -0600

    brcypt used to protect user password

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6] // Minimum 6 characters
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'user'
+    }  
+);
+
+module.exports = User;
